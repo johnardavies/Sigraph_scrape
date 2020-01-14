4b18b1ba19b3e0c3e599b17636c0c37cd548f198
johnardavies <johnardavies@gmail.com>
2020-01-14 19:51:00 +0000

    extract text and drop subnodes

--- sigraph_scrape/sigraph_scrape/spiders/sigraph_spider.py
+++ sigraph_scrape/sigraph_scrape/spiders/sigraph_spider.py
@@ -8,11 +8,11 @@ from sigraph_scrape.items import  SigraphScrapeItem
 class SiggraphSpider(scrapy.Spider):
     name = "siggraph"
     def __init__(self):
-        self.page_number=1 #initialises the class with a page number of 1
+        self.page_number=0 #initialises the class with a page number of 1
         
     def start_requests(self):
         print ('hello')
-        number_of_pages=2 #6 #This sets the number of pages that are looped through
+        number_of_pages=6 #6 #This sets the number of pages that are looped through
         for i in range(self.page_number, number_of_pages, 1):  #Loops through the pages between page_number and number_of_pages
          time.sleep(20) #Adds in a 5 second time delay 
          URL='https://digitalartarchive.siggraph.org/writings/papers/?pg='+str(i)+''
@@ -23,14 +23,15 @@ class SiggraphSpider(scrapy.Spider):
     def parse(self, response):
         hxs = Selector(response)
         print (hxs)
-        items=[]
-        followers=hxs.xpath('//p').extract()
+        items=[]  # /hr[]/following-sibling::text()
+        followers=hxs.xpath('//p[not(descendant::a)]/text()').extract()  #extract text between p tages exclude those containing nested a tags
         for elem in followers: #loops through the returned titles
            print(elem)
            item=SigraphScrapeItem()
-           #This extracts the userid from the hyperlink, probably better with a regex
-          # item["names"]=name
            item["text"]=elem
            items.append(item)#sticks the next returned list into the items list
         return items # returns the list
+
+
+
 
